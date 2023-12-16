e11062d2ddcc4370417e504048915d583d6dd99f
Zhang Lin <zlin1312138@gmail.com>
2023-12-16 16:36:05 -0700

    Corrected multithreading and wrote possible regions to json file

--- spotify.py
+++ spotify.py
@@ -36,6 +36,49 @@ def configure_logger(logger_name, log_file, level = logging.DEBUG) -> logging.Lo
 csvLogger = configure_logger('csvLogger', 'log/csv.log')
 scrapingLogger = configure_logger('spotifyLogger', 'log/spotify.log')
 
+def createDriver(): 
+    """ 
+        Web-drivers are not thread safe -- they sort of resemable a thread themselve
+        Therefore for each thread, we need to create a seperate webdriver
+    """
+    options = webdriver.ChromeOptions()
+    # The headless option doesn't seem to be supported for webscraping spotify blocks it?
+    # options.add_argument('--headless')
+    # options.add_argument('--disable-dev-shm-usage')
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+def addInput(driver: webdriver, by: By, value: str, text: str):
+    element = WebDriverWait(driver, 5).until(
+        EC.presence_of_element_located((by, value))
+    )
+    element.send_keys(text)
+
+#
+def clickButton(driver: webdriver, by: By, value: str):
+    button = WebDriverWait(driver, 5).until(
+        EC.element_to_be_clickable((by, value))
+    )
+    button.click()
+
+def loginSpotify(driver: webdriver): 
+    """ 
+        Spotify will redirect all attempts to webscraping to default login page. 
+        Use Selenium to dynamically login and continue webscraping all the Global charts INFO
+        loginSpotify() sends by username and password and clicks submit when 
+        fields are filled. 
+    """
+    username = config("SPOTIFY_USERNAME")
+    password = config("SPOTIFY_PASSWORD")
+
+    # Go to login page, wait for page to load, config and simulate login
+    driver.get("https://accounts.spotify.com/en/login")
+    addInput(driver, by=By.ID, value='login-username', text=username)
+    addInput(driver, by=By.ID, value='login-password', text=password)
+    clickButton(driver, by=By.ID, value='login-button')
+    # Delay for button click to be processed and login processed
+    sleep(3)
+
 def createDate(startDate: str, endDate: str, dateFormat: str = '%Y-%m-%d') -> list:
     """ 
         Purpose: Returns a list of dates between the 
@@ -88,7 +131,7 @@ def writeTop200Songs(input:str, filePath: str, mode: str, date: str, region: str
                 csvLogger.info(f"Writing to file {filePath} with '{mode}' status for {date} - {region}")
                 writer = csv.writer(file)
                 if(mode == 'w'): 
-                    header = ['date', 'region', 'positon', 'change', 'track_title', 'artist_name',
+                    header = ['date', 'region', 'position', 'change', 'track_title', 'artist_name',
                             'peak', 'prev', 'streak', 'streams']
                     writer.writerow(header)
                 # Write the data rows
@@ -121,74 +164,86 @@ def getChartElement(driver) -> str:
         scrapingLogger.fatal("chart element not found -- page not loaded properly and or invalid navigation links" + 
                              f'error message {str(e)}')
         
-def writeRegions(driver:webdriver):
-    '''
+def writeRegions():
+    ''' 
         Write each region/country available on spotify charts to regions.json 
         Format example follows the example; Argentina: [ar, 2017/01/2017]
         First item in array is the country abbreviation and second line a date
     '''
-    # Go to any spotify daily song chart page and specify a starting date
     # Note the starting date here is used to check if the region had Spotify at that time
-    # For example, South Korea only had Spotify until 2020 thus Spotify would redirect to 
-    # the first available date when clicking on it's dropdown menu
-    startDate = '2017/01/01'
-    url = 'https://charts.spotify.com/charts/view/regional-global-daily/{startDate}'
+    # ie if it exists the date would match else the first available date is returned
+    startDate = '2017-01-01'
+    url = f'https://charts.spotify.com/charts/view/regional-global-daily/{startDate}'
+    driver = createDriver()
+    loginSpotify(driver)
     driver.get(url)
     try: 
         # Click the dropdown menu for Regions
         clickButton(driver, by=By.ID, value='entity-search')
 
         # Wait for popUp container to appear
-        popUpContainer = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, 'popover-container'))
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//*[@data-popover-root="true"]'))
         )
-    except TimeoutException as e: 
-        scrapingLogger.fatal(str(e))
+        popUpContainer = driver.find_element(By.XPATH, '//*[@data-popover-root="true"]')
+        scrapingLogger.info('Pop Up Container located')
 
-    # Find all the items within the listbox
-    try:
-        listBox = popUpContainer.find_elements(By.XPATH, '//ul[@id="react-aria2894975124-2"]/li')
-
+        listBox = popUpContainer.find_elements(By.XPATH, '//ul[@role="listbox"]/li')
+        #Create list to help populate the dictionary for json file later on
         regionName = list()
         regionLink = list()
         # Loop through each li element and extract text and data-key attribute
         for liElement in listBox:
-            text = liElement.find_element(By.CLASS_NAME, 'EntityOptionDisplay__TextContainer-sc-1c8so1t-2').text
+            text = liElement.find_element(By.XPATH, './/*[contains(@class, "EntityOptionDisplay__TextContainer")]').text
             link = liElement.get_attribute('data-key')
             regionLink.append(link)
             regionName.append(text)
+        scrapingLogger.info(f'All region Names and links retrieved \nregionNames:{regionName} \nregionLink{regionLink}')
+
+        scrapingLogger.info("Begin parsing links for region.json")
+        regionDict = dict()
+        abbrPtrn = re.compile(r'(?<=-)\D+(?=-)')
+        datePtrn = re.compile(r'\d{4}-\d{2}-\d{2}')
+        for i, region in enumerate(regionName): 
+            regionDict[region] = list()
+            #Parse every link to get the region abbreviation and valid date
+            abbrResult  = abbrPtrn.search(regionLink[i])
+            dateResult = datePtrn.search(regionLink[i])
+            regionDict[region].append(abbrResult.group(0))
+            regionDict[region].append(dateResult.group(0))
+
+        with open('region.json', 'w')as json_file:
+            json.dump(regionDict, json_file, indent=2) 
+        scrapingLogger.info('Wrote Dictionary to region.json')
+    except TimeoutException as e: 
+        scrapingLogger.fatal(f'Pop Up container not found {str(e)}')
     except NoSuchElementException as e :
         scrapingLogger.fatal(str(e))
 
-    
 def validRegions(regions:dict) -> list:
     pass
 
 
-def chartHelper(driver, date, filePath, region='global', startingDate = '2017-01-01'):
+def chartHelper(driver: webdriver, dates:list):
     ''' 
-        Helper function -- used in chartHistory()
+        Helper function -- used to scrape a list of dates for multi-threading 
         Goes to spotify charts for a specific date and region and writes that to csv
+        -- Currently aims to scrape the global charts starting from 2017
     '''
     # Fill in the actual url appended with the specified region and dates
-    url = f'https://charts.spotify.com/charts/view/regional-{region}-daily/{date}'
+    filePath = "spotifyChartsNew2.csv"
+    region='global'
+    startingDate = '2017-01-01'
+
     # Navigate to SpotifyCharts and grab the top 200 songs for that day and region
-    driver.get(url)
-    scrapingLogger.info(f'Begin webscraping Daily Songs Chart for {date} {region}')
-    top200Songs = getChartElement(driver)
-    # Create file and write the first files with header -- append on subsequent iterations
-    writeTop200Songs(top200Songs, filePath, 'w' if date == startingDate else 'a', date, region)
-    scrapingLogger.info(f'Finished webscraping Daily Songs Chart for {date} {region}')
-
-def ChartHistory(driver, dates, filePath, region='global'):
-    ''' 
-        Multithreading to parallize the process each link is independent which 
-        significantly benefits from this -- speeds up process by n folds depending on hardware
-    '''
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Assuming you have a webdriver instance
-        executor.map(lambda date: chartHelper(driver, date, filePath, region), dates)
-
+    for date in dates: 
+        url = f'https://charts.spotify.com/charts/view/regional-{region}-daily/{date}'
+        driver.get(url)
+        scrapingLogger.info(f'Begin webscraping Daily Songs Chart for {date} {region}')
+        top200Songs = getChartElement(driver)
+        # Create file and write the first files with header -- append on subsequent iterations
+        writeTop200Songs(top200Songs, filePath, 'w' if date == startingDate else 'a', date, region)
+        scrapingLogger.info(f'Finished webscraping Daily Songs Chart for {date} {region}')
 
 def spotifyDebut():
     pass 
@@ -197,61 +252,32 @@ def artistRank():
     pass
 
 
-def addInput(driver: webdriver, by: By, value: str, text: str):
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((by, value))
-    )
-    element.send_keys(text)
-
-#
-def clickButton(driver: webdriver, by: By, value: str):
-    button = WebDriverWait(driver, 5).until(
-        EC.element_to_be_clickable((by, value))
-    )
-    button.click()
-
-def loginSpotify(driver: webdriver, username: str, password: str): 
-    """ 
-        Spotify will redirect all attempts to webscraping to default login page. 
-        Use Selenium to dynamically login and continue webscraping all the Global charts INFO
-        loginSpotify() sends by username and password and clicks submit when 
-        fields are filled. 
-    """
-    addInput(driver, by=By.ID, value='login-username', text=username)
-    addInput(driver, by=By.ID, value='login-password', text=password)
-    clickButton(driver, by=By.ID, value='login-button')
-
 def main(): 
 
-    # Setup driver and configure username and password
-    driver = webdriver.Chrome()
-    username = config("SPOTIFY_USERNAME")
-    password = config("SPOTIFY_PASSWORD")
+    jsonPath = 'region.json'
+    if(not os.path.exists(jsonPath)): 
+       writeRegions()
+    with open(jsonPath, 'r') as json_file:
+        regionDict = json.load(json_file)
+    print(regionDict)
 
-    # Go to login page, wait for page to config and simulate login
-    driver.get("https://accounts.spotify.com/en/login")
-    sleep(2) 
-    loginSpotify(driver, username, password)
-    sleep(2)
+    # #Initialize needed variables and split dates into n-thread-partitions 
+    # startDate = '2021-08-02'
+    # endDate = '2023-12-13'
+    # dates = createDate(startDate, endDate)
+    # #dates = ['2020-06-11','2021-01-12', '2021-02-26', '2021-01-20', '2021-07-06', '2021-04-28', '2021-07-26', '2021-09-21','2021-11-18']
+    # nThreads = 2
+    # nDates = len(dates)//nThreads
+    # dates = [dates[i * nDates:(i + 1) * nDates] for i in range(nThreads)]
+    # drivers = [createDriver() for i in range(nThreads)]
+    # [loginSpotify(driver) for driver in drivers]
 
-    filePath = "spotifyChartsTest.csv"
-    startDate = '2021-08-02'
-    endDate = '2023-12-13'
-    #dates = createDate(startDate, endDate)
-    dates = ['2017-01-01', '2021-01-12', '2021-01-20', '2021-02-26', '2021-04-28', '2021-07-06', '2021-07-26']
-    ChartHistory(driver, dates, filePath)
+    # with concurrent.futures.ThreadPoolExecutor(max_workers=nThreads) as executor:
+    #     executor.map(chartHelper, drivers, dates)
 
-    # jsonPath = 'region.json'
-    # if(~os.path.exists(jsonPath)): 
-    #    writeRegions()
-    # with open(jsonPath, 'r') as json_file:
-    #     regionDict = json.load(json_file)
-    
+    # [driver.quit() for driver in drivers]
 
-    
-    # Add a delay before quitting to ensure webscraping was performed
-    sleep(5)
-    driver.quit()
+
 
 
 if __name__ == "__main__":
